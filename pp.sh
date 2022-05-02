80baa2e7e2410924b50734846497d4299053c96b
Denys Mendez <denys@vmi868895.contaboserver.net>
2022-05-02 19:16:49 -0400

    Local config

--- pp.sh
+++ pp.sh
@@ -4,4 +4,6 @@ if [ "$5" = "xxx" ]; then
   mv -fv "$1" /mnt/media/others
 elif [ "$5" = "ximages" ]; then
   mv -fv "$1" /mnt/media/images
+elif [ "$5" = "erotics"]; then
+  mv -fv "$1" /mnt/media/erotics
 fi
